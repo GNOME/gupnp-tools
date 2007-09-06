a1357a1cf08de86e5765100d788b3d793a9318c9
Zeeshan Ali <zeenix@gmail.com>
2007-09-06 12:21:43 +0000

    Add context menu for, and ability to copy&paste from details treeview.
    git-svn-id: https://svn.o-hand.com/repos/gupnp/trunk/gupnp-tools@428 d8cb91d7-bff9-0310-92b9-80b65e4482b2

--- src/universal-cp-detailstreeview.c
+++ src/universal-cp-detailstreeview.c
@@ -25,6 +25,147 @@
 #include "universal-cp-detailstreeview.h"
 #include "universal-cp-gui.h"
 
+static gboolean
+get_selected_row (GtkTreeIter *iter)
+{
+        GtkWidget         *treeview;
+        GtkTreeModel      *model;
+        GtkTreeSelection  *selection;
+
+        treeview = glade_xml_get_widget (glade_xml, "details-treeview");
+        g_assert (treeview != NULL);
+        model = gtk_tree_view_get_model (GTK_TREE_VIEW (treeview));
+        g_assert (model != NULL);
+        selection = gtk_tree_view_get_selection (GTK_TREE_VIEW (treeview));
+        g_assert (selection != NULL);
+
+        return gtk_tree_selection_get_selected (selection, &model, iter);
+}
+
+static void
+setup_details_popup (GtkWidget *popup)
+{
+        GtkWidget *copy_value_menuitem;
+
+        copy_value_menuitem = glade_xml_get_widget (glade_xml,
+                                                    "copy-value-menuitem");
+        g_assert (copy_value_menuitem != NULL);
+
+        /* Only show "Copy Value" menuitem when a row is selected */
+        g_object_set (copy_value_menuitem,
+                      "visible",
+                      get_selected_row (NULL),
+                      NULL);
+}
+
+gboolean
+on_details_treeview_button_release (GtkWidget      *widget,
+                                    GdkEventButton *event,
+                                    gpointer        user_data)
+{
+        GtkWidget *popup;
+
+        if (event->type != GDK_BUTTON_RELEASE ||
+            event->button != 3)
+                return FALSE;
+
+        popup = glade_xml_get_widget (glade_xml, "details-popup");
+        g_assert (popup != NULL);
+
+        setup_details_popup (popup);
+
+        gtk_menu_popup (GTK_MENU (popup),
+                        NULL,
+                        NULL,
+                        NULL,
+                        NULL,
+                        event->button,
+                        event->time);
+        return TRUE;
+}
+
+void
+on_details_treeview_row_activate (GtkMenuItem *menuitem,
+                                  gpointer     user_data)
+{
+        GtkWidget    *treeview;
+        GtkTreeModel *model;
+        GtkTreeIter   iter;
+
+        treeview = glade_xml_get_widget (glade_xml, "details-treeview");
+        g_assert (treeview != NULL);
+        model = gtk_tree_view_get_model (GTK_TREE_VIEW (treeview));
+        g_assert (model != NULL);
+
+        if (get_selected_row (&iter)) {
+                char *val;
+
+                gtk_tree_model_get (model, &iter, 1, &val, -1);
+                if (val) {
+                        GtkClipboard *clipboard;
+
+                        clipboard = gtk_clipboard_get
+                                                (GDK_SELECTION_CLIPBOARD);
+                        g_assert (clipboard != NULL);
+                        gtk_clipboard_set_text (clipboard, val, -1);
+                        g_free (val);
+                }
+        }
+}
+
+void
+on_copy_all_details_activate (GtkMenuItem *menuitem,
+                              gpointer     user_data)
+{
+        GtkClipboard *clipboard;
+        GtkWidget    *treeview;
+        GtkTreeModel *model;
+        GtkTreeIter   iter;
+        char         *copied;
+
+        treeview = glade_xml_get_widget (glade_xml, "details-treeview");
+        g_assert (treeview != NULL);
+        model = gtk_tree_view_get_model (GTK_TREE_VIEW (treeview));
+        g_assert (model != NULL);
+        clipboard = gtk_clipboard_get (GDK_SELECTION_CLIPBOARD);
+        g_assert (clipboard != NULL);
+
+        if (!gtk_tree_model_get_iter_first (model, &iter))
+                return;
+
+        copied = g_strdup ("");
+        do {
+                char *name;
+                char *val;
+                char *pair;
+
+                gtk_tree_model_get (model, &iter,
+                                    0, &name,
+                                    1, &val, -1);
+                if (!name)
+                        continue;
+                if (!val) {
+                        g_free (name);
+                        continue;
+                }
+
+                pair = g_strjoin (" ", name, val, NULL);
+
+                g_free (val);
+                g_free (name);
+
+                val = copied;
+                copied = g_strjoin ("\n", copied, pair, NULL);
+
+                g_free (val);
+                g_free (pair);
+        } while (gtk_tree_model_iter_next (model, &iter));
+
+        gtk_clipboard_set_text (clipboard, copied, -1);
+
+        g_free (copied);
+}
+
 void
 update_details (const char **tuples)
 {
