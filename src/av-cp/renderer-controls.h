1a4671adf6dc0f7663091a05e269d809e5761899
Zeeshan Ali <zeenix@gmail.com>
2008-01-27 14:34:55 +0000

    Add the forgotten renderer-controls.h file to build and VCS.
    git-svn-id: https://svn.o-hand.com/repos/gupnp/trunk/gupnp-tools@710 d8cb91d7-bff9-0310-92b9-80b65e4482b2

---
+++ src/av-cp/renderer-controls.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2007 Zeeshan Ali <zeenix@gstreamer.net>
+ *
+ * Authors: Zeeshan Ali <zeenix@gstreamer.net>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __RENDERER_CONTROL_H__
+#define __RENDERER_CONTROL_H__
+
+#include <libgupnp/gupnp.h>
+#include <libgupnp-av/gupnp-av.h>
+#include <config.h>
+#include <gtk/gtk.h>
+#include <glade/glade.h>
+
+void
+add_media_renderer              (GUPnPDeviceProxy *proxy);
+
+void
+remove_media_renderer           (GUPnPDeviceProxy *proxy);
+
+void
+setup_renderer_combo            (GladeXML         *glade_xml);
+
+void
+set_av_transport_uri            (const char       *id,
+                                 const char       *uri,
+                                 GCallback         callback);
+
+#endif /* __RENDERER_CONTROL_H__ */
